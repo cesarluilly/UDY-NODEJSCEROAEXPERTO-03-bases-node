e10dea0448de36c754351836364428e9cf04be6c
cesarluilly <cesarluilly.garcia@hotmail.com>
2022-06-12 15:15:15 -0500

    Video 30 - Requerir paquetes require

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+const fs = require('fs');
 
 console.clear();
-const base = 5;
+//  //Vamos a ir a la documentacion de filesystem.
+//  //https://nodejs.org/dist/latest-v16.x/docs/api/fs.html#fswritefilefile-data-options-callback
+
+//  //Lo que queremos es grabar la tabla de multiplicar.
+
+const base = 3;
+let salida = '';
 
 console.log('=================================');
 console.log(`Tabla del ${base}`);
 console.log('=================================');
 
 for (let i = 0; i <= 10; i++) {
-    console.log(`${base } x ${i} = ${base * i}`);
+    salida += `${base } x ${i} = ${base * i}\n`;
 }
+
+console.log(salida);
+
+
+//  //Este metodo recibe un callback que se va a ejecutar cuando
+//  //  todo esto termine.
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+
+    console.log(`tabla-${base}.txt creada!`);
+    
+});
